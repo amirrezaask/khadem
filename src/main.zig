25e194a47cf3cf378dee9928106f592ac2eb9387
AmirrezaAsk <raskarpour@gmail.com>
2022-03-27 20:04:36 +0430

    Parse HTTP first line and headers

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const net = std.net;
 const StreamServer = net.StreamServer;
 const Address = net.Address;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
+const print = std.debug.print;
+
 pub const io_mode = .evented;
 
 /// Async Webserver : TCP Listener + HTTP protocol + handlers
@@ -9,6 +12,9 @@ pub const io_mode = .evented;
 /// 1. Library => create a webserver with config
 /// 2. Executable => gets a config file in YAML
 pub fn main() anyerror!void {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     var stream_server = StreamServer.init(.{});
     defer stream_server.close();
     const address = try Address.resolveIp("127.0.0.1", 8080);
@@ -16,11 +22,37 @@ pub fn main() anyerror!void {
 
     while (true) {
         const connection = try stream_server.accept();
-        try handler(connection.stream);
+        try handler(allocator, connection.stream);
     }
 }
 
-fn handler(stream: net.Stream) !void {
+fn handler(allocator: std.mem.Allocator, stream: net.Stream) !void {
+    _ = allocator;
     defer stream.close();
-    try stream.writer().print("Hello world from zig", .{});
+    var first_line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+    first_line = first_line[0..first_line.len];
+    var first_line_iter = std.mem.split(u8, first_line, " ");
+
+    const method = first_line_iter.next().?;
+    const uri = first_line_iter.next().?;
+    const version = first_line_iter.next().?;
+
+    var headers = std.StringHashMap([]const u8).init(allocator);
+
+    while (true) {
+        var line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+        if (line.len == 1 and std.mem.eql(u8, line, "\r")) break;
+        line = line[0..line.len];
+        var line_iter = std.mem.split(u8, line, ":");
+        const key = line_iter.next().?;
+        var value = line_iter.next().?;
+        if (value[0] == ' ') value = value[1..];
+        try headers.put(key, value);
+    }
+
+    print("method: {s}\nuri: {s}\nversion:{s}\n", .{ method, uri, version });
+    var headers_iter = headers.iterator();
+    while (headers_iter.next()) |entry| {
+        print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
 }
