1b0a779c9111097ed1e356051c61861a7b4ec267
AmirrezaAsk <raskarpour@gmail.com>
2022-04-21 23:37:24 +0430

    rustfmt

--- rust/src/http/server.rs
+++ rust/src/http/server.rs
@@ -1,11 +1,13 @@
-use tokio::net::{TcpStream, TcpListener};
-use async_trait::async_trait;
-use super::Error;
 use super::Connection;
+use super::Error;
+use async_trait::async_trait;
+use tokio::net::{TcpListener, TcpStream};
 
 #[async_trait]
 pub trait HttpHandler: Send + Sync + 'static {
-    async fn handle<CH>(&self, socket: TcpStream, ch: &CH) -> Result<(), Error> where CH: ConnectionHandler + Send + Sync;
+    async fn handle<CH>(&self, socket: TcpStream, ch: &CH) -> Result<(), Error>
+    where
+        CH: ConnectionHandler + Send + Sync;
 }
 
 #[async_trait]
@@ -13,24 +15,26 @@ pub trait ConnectionHandler {
     async fn handle_connection(&self, conn: &mut Connection) -> Result<(), Error>;
 }
 
-
-pub struct Server {
-    
-}
-
+pub struct Server {}
 
 impl Server {
     pub fn new() -> Server {
-        Server{}
+        Server {}
     }
 }
 
-pub struct LogMiddleware<CH> where CH: ConnectionHandler {
+pub struct LogMiddleware<CH>
+where
+    CH: ConnectionHandler,
+{
     pub wrapped: CH,
 }
 
 #[async_trait]
-impl <CH>ConnectionHandler for LogMiddleware<CH> where CH: ConnectionHandler + Sync + Send {
+impl<CH> ConnectionHandler for LogMiddleware<CH>
+where
+    CH: ConnectionHandler + Sync + Send,
+{
     async fn handle_connection(&self, connection: &mut Connection) -> Result<(), Error> {
         println!(
             "method: {:?}\nuri:{:?}\nversion:{:?}\nheaders:{:?}\n",
@@ -44,21 +48,24 @@ impl <CH>ConnectionHandler for LogMiddleware<CH> where CH: ConnectionHandler + S
     }
 }
 
-
-
 // parse http, create connection, request, call all internal logic, write response
 #[async_trait]
 impl HttpHandler for Server {
-    async fn handle<CH>(&self, socket: TcpStream, ch: &CH) -> Result<(), Error> where CH: ConnectionHandler + Send + Sync {
+    async fn handle<CH>(&self, socket: TcpStream, ch: &CH) -> Result<(), Error>
+    where
+        CH: ConnectionHandler + Send + Sync,
+    {
         let mut connection = Connection::new(socket).await?;
         ch.handle_connection(&mut connection).await;
         Ok(())
     }
 }
 
-
 impl Server {
-    pub async fn start<H>(addr: &str, handler: H) -> Result<(), Error> where H: ConnectionHandler + Send + Sync {
+    pub async fn start<H>(addr: &str, handler: H) -> Result<(), Error>
+    where
+        H: ConnectionHandler + Send + Sync,
+    {
         let listener = TcpListener::bind(addr).await?;
         let server = Server::new();
         loop {
@@ -67,4 +74,3 @@ impl Server {
         }
     }
 }
-
